32b32771bf615766783e03a89dddc52550ee58fb
Sergey Shishkin <shishkinsa1997@gmail.com>
2025-06-29 12:53:41 +0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-https://shishkinsa997.github.io/esoft/react_tasks/index.html
--- react_tasks/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/esoft/', // Укажите название репозитория
-  plugins: [react()]
-  },
-)
